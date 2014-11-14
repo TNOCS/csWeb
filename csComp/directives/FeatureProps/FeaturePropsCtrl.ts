e93db46bbf728d4a582a92dc247442165f024fb1
Erik Vullings <erik.vullings@gmail.com>
2014-11-14 09:44:52 +0100

    StringExt --> csComp.StringExt.

--- csComp/directives/FeatureProps/FeaturePropsCtrl.ts
+++ csComp/directives/FeatureProps/FeaturePropsCtrl.ts
@@ -2,7 +2,6 @@
     import IFeature     = csComp.GeoJson.IFeature;
     import IFeatureType = csComp.GeoJson.IFeatureType;
     import IMetaInfo    = csComp.GeoJson.IMetaInfo;
-    import StringExt    = csComp.StringExt;
 
     class FeaturePropsOptions implements L.SidebarOptions {
         public position: string;
@@ -62,7 +61,7 @@
             this.sectionIcon = sectionIcon;
         }
 
-        public showSectionIcon(): boolean { return !StringExt.isNullOrEmpty(this.sectionIcon); }
+        public showSectionIcon(): boolean { return !csComp.StringExt.isNullOrEmpty(this.sectionIcon); }
 
         public addProperty(key: string, value: string, property: string, canFilter: boolean, canStyle: boolean, feature: IFeature, isFilter : boolean,description?: string, meta?: IMetaInfo ): void {
             
@@ -109,21 +108,21 @@
                     var callOutSection = this.getOrCreateCallOutSection(mi.section) || infoCallOutSection;
                     callOutSection.metaInfos[mi.label] = mi;
                     var text = feature.properties[mi.label];
-                    if (!StringExt.isNullOrEmpty(text) && !$.isNumeric(text))
+                    if (!csComp.StringExt.isNullOrEmpty(text) && !$.isNumeric(text))
                         text = text.replace(/&amp;/g, '&');
                     //if (mi.stringFormat)
                     //    text = StringExt.format(mi.stringFormat, text);
-                    if (StringExt.isNullOrEmpty(text)) return;
+                    if (csComp.StringExt.isNullOrEmpty(text)) return;
                     switch (mi.type) {
                         case "bbcode":
-                            if (!StringExt.isNullOrEmpty(mi.stringFormat))
+                            if (!csComp.StringExt.isNullOrEmpty(mi.stringFormat))
                                 text = String.format(mi.stringFormat, text);
                             displayValue = XBBCODE.process({ text: text }).html;
                         break;
                     case "number":
                         if (!$.isNumeric(text))
                             displayValue = text;
-                        else if (StringExt.isNullOrEmpty(mi.stringFormat))
+                        else if (csComp.StringExt.isNullOrEmpty(mi.stringFormat))
                             displayValue = text.toString();
                         else
                             displayValue = String.format(mi.stringFormat, parseFloat(text));
@@ -133,7 +132,7 @@
                         break;
                     }
                     // Skip empty, non-editable values
-                    if (!mi.canEdit && StringExt.isNullOrEmpty(displayValue)) return;
+                    if (!mi.canEdit && csComp.StringExt.isNullOrEmpty(displayValue)) return;
 
 
                     var canFilter = (mi.type == "number" || mi.type == "text");
@@ -172,7 +171,7 @@
         //}
 
         private getOrCreateCallOutSection(sectionTitle: string): ICallOutSection {
-            if (StringExt.isNullOrEmpty(sectionTitle)) {
+            if (csComp.StringExt.isNullOrEmpty(sectionTitle)) {
                 return null;
             }
             if (sectionTitle in this.sections)
@@ -186,11 +185,11 @@
          */
         private setTitle() {
             var title: string;
-            if (this.type == null || this.type.style == null || StringExt.isNullOrEmpty(this.type.style.nameLabel))
+            if (this.type == null || this.type.style == null || csComp.StringExt.isNullOrEmpty(this.type.style.nameLabel))
                 title = this.feature.properties['Name'];
             else
                 title = this.feature.properties[this.type.style.nameLabel];
-            if (!StringExt.isNullOrEmpty(title) && !$.isNumeric(title))
+            if (!csComp.StringExt.isNullOrEmpty(title) && !$.isNumeric(title))
                 this.title = title.replace(/&amp;/g, '&');
         }
     }
