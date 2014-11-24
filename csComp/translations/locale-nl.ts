3dfc2d1485deae1911b2770f73748bf935866866
Erik Vullings <erik.vullings@gmail.com>
2014-11-24 18:53:02 +0100

    Added translations to csComp
    Instead of updating the language translations in app.ts, they are now
    being loaded from csComp.js. However, it is still possible to add
    specific translations to app.ts.

---
+++ csComp/translations/locale-nl.ts
@@ -0,0 +1,41 @@
+﻿module Translations {
+    export class Dutch {
+        public static locale: ng.translate.ITranslationTable = {
+            MAP              : 'Kaarten',
+            MAP_LABEL        : 'Kaart',
+            TABLE_LABEL      : 'Tabel',
+            LAYERS           : 'Kaartlagen',
+            FILTERS          : 'Filters',
+            FILTER_INFO      : 'Momenteel zijn er geen filters geselecteerd. Klik op een icoon of gebied op de kaart, en klik op het filter icoontje (<span class="fa fa-filter"></span>) in het rechter menu om een filter toe te voegen. Dan wordt er een filter aangemaakt voor de geselecteerde eigenschap.',
+            STYLES           : 'Stijlen',
+            STYLE_INFO       : 'Momenteel zijn er geen stijlen geselecteerd. Klik op een icoon of gebied op de kaart, en klik op het stijl icoontje (<span class="fa fa-eye"></span>) in het rechter menu om een stijl toe te voegen. Dan wordt er een stijl aangemaakt voor de geselecteerde eigenschap.',
+            FEATURES         : 'Features',
+            LEGEND           : 'Legenda',
+            SEARCH           : 'Zoeken',
+            MAP_FEATURES     : 'Kaartfeatures',
+            WHITE_RED        : 'wit - rood',
+            RED_WHITE        : 'rood - wit',
+            GREEN_RED        : 'groen - rood',
+            RED_GREEN        : 'rood - groen',
+            WHITE_BLUE       : 'wit - blauw',
+            BLUE_WHITE       : 'wit - groen',
+            WHITE_GREEN      : 'wit - groen',
+            GREEN_WHITE      : 'groen - wit',
+            WHITE_ORANGE     : 'wit - oranje',
+            ORANGE_WHITE     : 'oranje - wit',
+            SHOW_FEATURE_MSG : 'Selecteer een feature op de kaart om de Multi-Criteria Analyse (MCA) resultaten in detail te bekijken.',
+            MCA_TOTAL_RESULT : 'Gecombineerd resultaat',
+            MCA_DELETE_MSG   : 'Verwijder "{0}"',
+            MCA_DELETE_MSG2  : 'Weet u het zeker?',
+            //MCA_COMPUTE_MGS: "Bereken de '{{mcaTitle}}'.",
+            SHOW5            : 'Toon 5 regels',
+            SHOW10           : 'Toon 10 regels',
+            SHOW15           : 'Toon 15 regels',
+            SHOW20           : 'Toon 20 regels',
+            SHOW25           : 'Toon 25 regels',
+            SHOW30           : 'Toon 30 regels',
+            SHOW35           : 'Toon 35 regels',
+            SHOW40           : 'Toon 40 regels'
+        }
+    }
+}
