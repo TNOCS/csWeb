353fc242697a89b08d8680666aa401fe5a108718
Erik Vullings <erik.vullings@gmail.com>
2014-12-13 16:43:00 +0100

    MCA: Changed " to '

--- csComp/directives/MCA/Mca.ts
+++ csComp/directives/MCA/Mca.ts
@@ -1,10 +1,10 @@
 ﻿module Mca {
-    "use strict";
+    'use strict';
 
     /**
      * Config
      */
-    var moduleName = "csWeb.mca";
+    var moduleName = 'csWeb.mca';
 
     /**                    
      * Module
@@ -12,7 +12,7 @@
     export var myModule : ng.IModule;
     try {
         myModule = angular.module(moduleName);     
-    } catch (err) { 
+    } catch (err) {
         // named module does not exist, so create one
         myModule = angular.module(moduleName, []);
     }
@@ -21,16 +21,16 @@
      * Directive to display an MCA control.
      */
     myModule
-        .directive("mca", [
-            "$window", "$compile", "$templateCache",  
+        .directive('mca', [
+            '$window', '$compile', '$templateCache',  
             function ($window, $compile, $templateCache): ng.IDirective {
                 return {   
                     terminal: true,  // do not compile any other internal directives 
-                    restrict: "EA",  // E = elements, other options are A=attributes and C=CSS classes
+                    restrict: 'EA',  // E = elements, other options are A=attributes and C=CSS classes
                     scope: {},       // isolated scope, separated from parent. Is however empty, as this directive is self contained by using the messagebus.
                     template: html,  // I use gulp automatian to compile the FeatureProperties.tpl.html to a simple TS file, FeatureProperties.tpl.ts, which contains the html as string. The advantage is that you can use HTML intellisence in the html file.
                     compile: el => { // I need to explicitly compile it in order to use interpolation like {{xxx}}
-                        $templateCache.put("mcaEditorView.html", McaEditorView.html);
+                        $templateCache.put('mcaEditorView.html', McaEditorView.html);
                         var fn = $compile(el);
                         return scope => {
                             fn(scope);
