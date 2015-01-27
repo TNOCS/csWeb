2f27ca40bcb559a61d2d68dec56c887d4165c7c1
erik <erik.vullings@gmail.com>
2015-01-27 12:40:51 +0100

    Updated project to TypeScript 1.4

--- csCompTests/specifications.js
+++ csCompTests/specifications.js
@@ -1,4 +1,4 @@
-﻿var _this = this;
+var _this = this;
 describe('The MCA model ', function () {
     beforeEach(function () {
         _this.mca = new Mca.Models.Mca();
@@ -6,27 +6,21 @@ describe('The MCA model ', function () {
         _this.mca.criteria.push({ userWeight: 3, criteria: [] });
         _this.mca.criteria.push({ userWeight: 2, criteria: [] });
     });
-
     it('should calculate the correct weights based on the user weights.', function () {
         _this.mca.calculateWeights();
-
         expect(_this.mca.criteria[0].weight).toBe(0.5);
         expect(_this.mca.criteria[1].weight).toBe(0.3);
         expect(_this.mca.criteria[2].weight).toBe(0.2);
     });
-
     it('should assign a color to each criterion after updating.', function () {
         expect(_this.mca.criteria[0].color != null).toBe(false);
         expect(_this.mca.criteria[1].color != null).toBe(false);
         expect(_this.mca.criteria[2].color != null).toBe(false);
-
         _this.mca.update();
-
         expect(_this.mca.criteria[0].color != null).toBe(true);
         expect(_this.mca.criteria[1].color != null).toBe(true);
         expect(_this.mca.criteria[2].color != null).toBe(true);
     });
-
     it('should use the minValue and maxValue when provided for computing the PLA', function () {
         var features = [
             {
@@ -49,7 +43,6 @@ describe('The MCA model ', function () {
             }
         ];
         var selectedFeature = features[1];
-
         var criterion = new Mca.Models.Criterion();
         criterion.label = 'test';
         criterion.scores = Mca.Models.ScoringFunction.createScores(1 /* Ascending */);
@@ -57,9 +50,7 @@ describe('The MCA model ', function () {
         criterion.isPlaScaled = true;
         criterion.updatePla(features);
         var result = Math.round(criterion.getScore(selectedFeature) * 1000);
-
         expect(result).toBe(292);
-
         criterion = new Mca.Models.Criterion();
         criterion.label = 'test';
         criterion.scores = Mca.Models.ScoringFunction.createScores(1 /* Ascending */);
@@ -69,10 +60,8 @@ describe('The MCA model ', function () {
         criterion.isPlaScaled = true;
         criterion.updatePla(features);
         result = criterion.getScore(selectedFeature);
-
         expect(result).toBe(0.4);
     });
-
     it('should return a result of 0 when the value is outside the cut-off range.', function () {
         var features = [
             {
@@ -95,23 +84,19 @@ describe('The MCA model ', function () {
             }
         ];
         var selectedFeature = features[0];
-
         var criterion = new Mca.Models.Criterion();
         criterion.label = 'test';
         criterion.userWeight = 1;
         criterion.isPlaScaled = true;
         criterion.scores = Mca.Models.ScoringFunction.createScores(1 /* Ascending */);
-
         criterion.minValue = 2;
         criterion.maxValue = 7;
         criterion.updatePla(features);
         var result = criterion.getScore(selectedFeature);
         expect(result).toBe(1);
-
         criterion.maxCutoffValue = 8;
         result = criterion.getScore(selectedFeature);
         expect(result).toBe(0);
-
         selectedFeature = features[2];
         criterion.minValue = 1;
         criterion.maxValue = 11;
