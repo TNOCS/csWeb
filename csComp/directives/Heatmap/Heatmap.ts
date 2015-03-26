edebd9623b6b59f2dcac0a144c412c68a0c2f74f
unknown <rbruining@ALTEN-L171.alten.nl>
2015-03-26 17:08:49 +0100

    Added heatmap implementation (does not use new layers yet)

---
+++ csComp/directives/Heatmap/Heatmap.ts
@@ -0,0 +1,45 @@
+module Heatmap {
+    'use strict';
+
+    /**
+     * Config
+     */
+    var moduleName = 'csWeb.heatmap';
+
+    /**
+     * Module
+     */
+    export var myModule : ng.IModule;
+    try {
+        myModule = angular.module(moduleName);
+    } catch (err) {
+        // named module does not exist, so create one
+        myModule = angular.module(moduleName, []);
+    }
+
+    /**
+     * Directive to display a heatmap control.
+     */
+    myModule
+        .directive('heatmap', [
+            '$window', '$compile', '$templateCache',
+            function ($window, $compile, $templateCache): ng.IDirective {
+                return {
+                    terminal: true,  // do not compile any other internal directives
+                    restrict: 'EA',  // E = elements, other options are A=attributes and C=CSS classes
+                    scope: {},       // isolated scope, separated from parent. Is however empty, as this directive is self contained by using the messagebus.
+                    template: html,  // I use gulp automatian to compile the FeatureProperties.tpl.html to a simple TS file, FeatureProperties.tpl.ts, which contains the html as string. The advantage is that you can use HTML intellisence in the html file.
+                    compile: el => { // I need to explicitly compile it in order to use interpolation like {{xxx}}
+                        $templateCache.put('heatmapEditorView.html', HeatmapEditorView.html);
+                        var fn = $compile(el);
+                        return scope => {
+                            fn(scope);
+                        };
+                    },
+                    replace: true,    // Remove the directive from the DOM
+                    transclude: true, // Add elements and attributes to the template
+                    controller: HeatmapCtrl
+                }
+            }
+        ]);
+}
