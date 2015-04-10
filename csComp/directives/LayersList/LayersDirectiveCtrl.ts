e642d3387261b2decab0130036fb7c92e50faa43
erik <erik.vullings@gmail.com>
2015-04-10 16:26:00 +0200

    Project was not building in VS anymore: order of files in vs project file was wrong (project.ts needs to be compiled before expert directive in order to use expertise enum). Removed unused projects (csCompServer and csTest). NB: Also updated angularjs to version 1.3.15: because of this, the toggleLayer behaviour is changed!!!.

--- csComp/directives/LayersList/LayersDirectiveCtrl.ts
+++ csComp/directives/LayersList/LayersDirectiveCtrl.ts
@@ -28,7 +28,7 @@
             //Uncommented after updating the bower packages: the behaviour changed between angular versions.
             // layer.enabled = !layer.enabled;
             // Unselect when dealing with a radio group, so you can turn a loaded layer off again.
-            if (layer.group.oneLayerActive && this.$layerService.findLayer(layer.id)) layer.enabled = false;
+            //if (layer.group.oneLayerActive && this.$layerService.findLayer(layer.id)) layer.enabled = false;
             
             if (layer.enabled) {    
                 this.$layerService.addLayer(layer);
