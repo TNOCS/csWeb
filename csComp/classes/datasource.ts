bd8a6f6c7c50e06fb81885b93ada65be1e287227
Arnoud <damylen@gmail.com>
2015-05-04 11:31:45 +0200

    SensorSet can contain propertyTypeKey that references property description in project.json propertyTypeData collection

--- csComp/classes/datasource.ts
+++ csComp/classes/datasource.ts
@@ -4,6 +4,7 @@
         id: string;
         title: string;
         type: string;
+        propertyTypeKey : string;
         timestamps: number[];
         values: any[];
         activeValue: any;
@@ -24,6 +25,13 @@
         title: string;
         sensors: { (key: string): SensorSet };
 
+        static merge_sensor(s1 : SensorSet,s2 : SensorSet) : SensorSet {
+          var obj3 : SensorSet = new SensorSet();
+          for (var attrname in s1) { obj3[attrname] = s1[attrname]; }
+          for (var attrname in s2) { obj3[attrname] = s2[attrname]; }
+          return obj3;
+        }
+
         /**
          * Load JSON data.
          * @type {DataSource}
@@ -36,6 +44,7 @@
                 $.getJSON(ds.url,(temp: DataSource) => {
                     if (temp != null) {
                         ds.id      = temp.id;
+                        for (var s in temp.sensors) { if (temp.sensors.hasOwnProperty(s)) ds.sensors[s] = this.merge_sensor(ds.sensors[s],temp.sensors[s]); }
                         ds.sensors = temp.sensors;
                         ds.title   = temp.title;
                         callback();
