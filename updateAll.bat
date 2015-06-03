535c8bfb2d5e8308466bb111df1d32e88e8b6364
erik <erik.vullings@gmail.com>
2015-06-03 15:59:26 +0200

    Added overall updateAll.bat script.

---
+++ updateAll.bat
@@ -0,0 +1,14 @@
+rem @echo off
+REM Run npm and bower update, and start gulp
+REM As they are all commands, I need to call them, otherwise the script will exit automatically after each program.
+cd csServerComp
+call tsc
+cd ../csComp
+call npm update
+call tsc
+cd ../example
+call npm update
+cd public
+call bower update
+cd ..
+call tsc
