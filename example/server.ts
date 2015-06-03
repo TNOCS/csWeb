29c656b981f4c69f8f3a693255f4bd4cdf30b241
erik <erik.vullings@gmail.com>
2015-06-03 17:57:59 +0200

    Upgrade to express 4.

--- example/server.ts
+++ example/server.ts
@@ -21,7 +21,10 @@ import DynamicProject       = require("ServerComponents/dynamic/DynamicProject")
 // });
 
 // setup socket.io object
+var favicon = require('serve-favicon');
+var bodyParser = require('body-parser')
 var server        = express();
+
 var httpServer    = require('http').Server(server);
 var cm            = new cc.ConnectionManager(httpServer);
 var messageBus    = new MessageBus.MessageBusService();
@@ -31,14 +34,9 @@ var config        = new ConfigurationService('./configuration.json');
 // all environments
 var port = "3002";
 server.set('port', port);
-server.set('views', path.join(__dirname, 'views'));
-server.set('view engine', 'jade');
-server.use(express.favicon());
-server.use(express.logger('dev'));
-server.use(express.json());
-server.use(express.urlencoded());
-server.use(express.methodOverride());
-server.use(server.router);
+server.use(favicon(__dirname + '/public/favicon.ico'));
+server.use(bodyParser.json()); // support json encoded bodies
+server.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 config.add("server", "http://localhost:" + port);
 
@@ -54,23 +52,11 @@ ds.Start();
 server.get("/datasource", ds.GetDataSource);
 
 var bagDatabase = new BagDatabase(config);
-// server.get(config["resolveAddress"], (req, res) => bagDatabase.lookupAddress(req, res));
 var mapLayerFactory = new MapLayerFactory(bagDatabase, messageBus);
 server.post('/projecttemplate', (req, res) => mapLayerFactory.process(req, res));
 
 server.use(express.static(path.join(__dirname, 'public')));
 console.log("started");
-
-// development only
-if ('development' == server.get('env')) {
-    server.use(express.errorHandler());
-}
-
-// server.get('/', (req, res) => {
-//    res.render('index.html');
-// });
-//server.get('/', routes.index);
-//server.get('/users', user.list);
 
 httpServer.listen(server.get('port'),() => {
     console.log('Express server listening on port ' + server.get('port'));
