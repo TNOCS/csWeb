cff2dfe2dc46bed64bce335aa5bb86992362b6fb
Rinze <rinze.bruining@tno.nl>
2015-06-09 15:12:20 +0200

    Added test with mocked JSON datasource

--- csComp/classes/datasource.ts
+++ csComp/classes/datasource.ts
@@ -1,40 +1,39 @@
 ﻿module csComp.Services {
 
     export class SensorSet {
-        id: string;
-        title: string;
-        type: string;
-        propertyTypeKey : string;
-        propertyType : IPropertyType;
-        timestamps: number[];
-        values: any[];
-        activeValue: any;
+        id:              string;
+        title:           string;
+        type:            string;
+        propertyTypeKey: string;
+        propertyType:    IPropertyType;
+        timestamps:      number[];
+        values:          any[];
+        activeValue:     any;
 
-        public activeValueText() : string {
-          return Helpers.convertPropertyInfo(this.propertyType,this.activeValue);
+        public activeValueText(): string {
+            return Helpers.convertPropertyInfo(this.propertyType, this.activeValue);
         }
 
-        public addValue(date : number, value : number)
-        {
-          this.timestamps.push(date);
-          this.values.push(value);
-          this.activeValue = value;
+        public addValue(date: number, value: number) {
+            this.timestamps.push(date);
+            this.values.push(value);
+            this.activeValue = value;
         }
     }
 
     export class DataSource {
-        id: string;
-        url: string;
+        id:      string;
+        url:     string;
         /** static, dynamic */
-        type : string;
-        title: string;
+        type:    string;
+        title:   string;
         sensors: { (key: string): SensorSet };
 
-        static merge_sensor(s1 : SensorSet,s2 : SensorSet) : SensorSet {
-          var obj3 : SensorSet = new SensorSet();
-          for (var attrname in s1) { obj3[attrname] = s1[attrname]; }
-          for (var attrname in s2) { obj3[attrname] = s2[attrname]; }
-          return obj3;
+        static merge_sensor(s1: SensorSet, s2: SensorSet): SensorSet {
+            var obj3: SensorSet = new SensorSet();
+            for (var attrname in s1) { obj3[attrname] = s1[attrname]; }
+            for (var attrname in s2) { obj3[attrname] = s2[attrname]; }
+            return obj3;
         }
 
         /**
@@ -46,14 +45,19 @@
          */
         public static LoadData(ds: DataSource, callback: Function) {
             if (ds.url != null) {
-                $.getJSON(ds.url,(temp: DataSource) => {
+                $.getJSON(ds.url, (temp: DataSource) => {
                     if (temp != null) {
-                        ds.id      = temp.id;
-                        for (var s in temp.sensors) {
-                            if (temp.sensors.hasOwnProperty(s))
-                                ds.sensors[s] = this.merge_sensor(ds.sensors[s], temp.sensors[s]);
+                        ds.id = temp.id;
+                        if (!ds.hasOwnProperty('sensors')) {
+                            ds.sensors = temp.sensors;
+                        } else {
+                            for (var s in temp.sensors) {
+                                if (temp.sensors.hasOwnProperty(s)) {
+                                    ds.sensors[s] = this.merge_sensor(ds.sensors[s], temp.sensors[s]);
+                                }
+                            }
                         }
-                        ds.title   = temp.title;
+                        ds.title = temp.title;
                         callback();
                     }
                 });
