bc9334a2cfc6715cf13da4fdbbbb42bcae11492e
erik <erik.vullings@gmail.com>
2015-06-18 16:00:16 +0200

    * Extended Date class with yyyymmdd method.

--- csComp/services/layer/sources/NightDayDataSource.ts
+++ csComp/services/layer/sources/NightDayDataSource.ts
@@ -10,6 +10,10 @@
 interface Date {
     getJulian(): number;
     getGMST(): number;
+    /**
+     * Get date in YYYYMMDD format
+     */
+    yyyymmdd(): string;
 }
 
 Date.prototype.getJulian = function() {
@@ -26,6 +30,13 @@ Date.prototype.getGMST = function() {
     var d = julianDay - 2451545.0;
     // Low precision equation is good enough for our purposes.
     return (18.697374558 + 24.06570982441908 * d) % 24;
+}
+
+Date.prototype.yyyymmdd = function() {
+    var yyyy = this.getFullYear().toString();
+    var mm = (this.getMonth()+1).toString(); // getMonth() is zero-based
+    var dd  = this.getDate().toString();
+    return yyyy + (mm[1]?mm:"0"+mm[0]) + (dd[1]?dd:"0"+dd[0]); // padding
 }
 
 declare module L {
