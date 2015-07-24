6fc3deaf58134c3b896bcbd57f372d979573bb82
Rinze <rbruining@ALTEN-L171.alten.nl>
2015-07-24 17:14:32 +0200

    - Dropdown section menu in FeatureProps on hover instead of click. - Updated leaflet.d.ts with union instead of any - BAG: Added option to get number of 'Verblijfsobjecten' in 'Pand' (was requested for determining woningtype)  + support for houseLetter in address search

--- example/Scripts/typings/leaflet/leaflet.d.ts
+++ example/Scripts/typings/leaflet/leaflet.d.ts
@@ -345,7 +345,7 @@ declare module L {
         /**
          * The maximum radius that a cluster will cover from the central marker (in pixels). Default 80. Decreasing will make more smaller clusters. You can also use a function that accepts the current map zoom and returns the maximum cluster radius in pixels.
          */
-        maxClusterRadius?: any;
+        maxClusterRadius?: ((number) => number) | number;
         /**
          *  Increase from 1 to increase the distance away from the center that spiderfied markers are placed. Use if you are using big marker icons (Default:1)
          */
