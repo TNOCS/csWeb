c8b6ce2b4a5437942ea1cecf1561f2ac1920e057
Carlos Martinez <c.martinez@esciencecenter.nl>
2015-08-27 16:43:00 +0200

    HTTP request refactor for OfflineSearchCtrl (no tests)

--- csComp/directives/Editors/LayerEditor/LayerEditCtrl.ts
+++ csComp/directives/Editors/LayerEditor/LayerEditCtrl.ts
@@ -67,7 +67,6 @@ module LayerEdit {
                             this.$layerService._featureTypes[id] = ft;
                             r.featureTypes[ft.id] = ft;
                         }
-                            //this.$layerService.getFeatureTypeId()
                     }
                 });
             }
