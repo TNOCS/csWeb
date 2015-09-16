4c09e7addc44bfc0caff189396f0380c6ac0a6e8
Erik Vullings <erik.vullings@gmail.com>
2015-09-16 13:51:29 +0200

    Added getDirectories method in Utils class.

--- csServerComp/ServerComponents/helpers/Utils.ts
+++ csServerComp/ServerComponents/helpers/Utils.ts
@@ -1,3 +1,7 @@
+var fs = require('fs'),
+    path = require('path');
+
+/** Create a new GUID */
 export function newGuid() {
     var guid = (this.S4() + this.S4() + "-" + this.S4() + "-4" + this.S4().substr(0, 3) + "-" + this.S4() + "-" + this.S4() + this.S4() + this.S4()).toLowerCase();
     return guid;
@@ -5,4 +9,11 @@ export function newGuid() {
 
 export function S4() {
     return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+}
+
+/** Get all the directories in a folder. */
+export function getDirectories(srcpath: string): string[] {
+  return fs.readdirSync(srcpath).filter(function(file: string) {
+    return fs.statSync(path.join(srcpath, file)).isDirectory();
+  });
 }
