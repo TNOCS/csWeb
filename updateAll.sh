25b3619e70604fbea2c00702b38aecea2c448fd0
mkuzak <mateusz.kuzak@gmail.com>
2015-10-28 16:54:51 +0100

    Allow root for bower in updateAll

--- updateAll.sh
+++ updateAll.sh
@@ -2,7 +2,7 @@ tsc -p csServerComp
 (cd csComp && npm install)
 tsc -p csComp
 (cd example && npm install)
-(cd example/public && bower install)
+(cd example/public && bower --allow-root install --config.interactive=false)
 (cd example/ && gulp all)
 tsc -p example
 (cd example/ && gulp built_csComp.d.ts)
