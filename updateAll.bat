1d68000cce61dca27fc708b1486e21fb8d7c1f9a
Erik Vullings <erik.vullings@gmail.com>
2015-10-28 14:03:44 +0100

    csComp does not need a node_modules folder. Removed it.

--- updateAll.bat
+++ updateAll.bat
@@ -3,8 +3,8 @@ REM Run npm and bower update, and start gulp
 REM As they are all commands, I need to call them, otherwise the script will exit automatically after each program.
 cd csServerComp
 call tsc
-cd ../csComp
-call npm install
+REM cd ../csComp
+REM call npm install
 call tsc
 cd ../example
 call npm install
