eeeb0db6b53f2ed9c77c2dbd404393e8d3dcd52e
Carlos Martinez <c.martinez@esciencecenter.nl>
2015-11-02 14:42:56 +0100

    init task on gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp          = require('gulp');
 var tsconfig      = require('gulp-tsconfig-files');
+var tsd           = require('gulp-tsd');
 var exec          = require('child_process').exec;
 var install       = require('gulp-install');
 
@@ -13,6 +14,14 @@ function run(command, cb) {
     cb(err);
   });
 }
+
+// This task runs tsd command on csComp folder
+gulp.task('comp_tsd', function(cb) {
+  tsd({
+    command: 'reinstall',
+    config: 'csComp/tsd.json',
+  }, cb);
+});
 
 // This task updates the typescript dependencies on tsconfig file for csComp
 gulp.task('comp_tsconfig_files', function() {
@@ -35,6 +44,14 @@ gulp.task('comp_tsc', function(cb) {
   run('tsc -p csComp', cb);
 });
 
+// This task runs tsd command on csServerComp folder
+gulp.task('servercomp_tsd', function(cb) {
+  tsd({
+    command: 'reinstall',
+    config: 'csServerComp/tsd.json',
+  }, cb);
+});
+
 // This task updates the typescript dependencies on tsconfig file for csServerComp
 gulp.task('servercomp_tsconfig_files', function() {
   gulp.src(['csServerComp/**/*.ts',
@@ -55,11 +72,9 @@ gulp.task('servercomp_tsc', function(cb) {
   run('tsc -p csServerComp', cb);
 });
 
-// Run required npm and bower installs
-gulp.task('install', function() {
+// Run required npm and bower installs for example folder
+gulp.task('example_deps', function() {
   gulp.src([
-      'csComp/tsd.json',            // tsd install
-      'csServerComp/tsd.json',      // tsd install
       'example/package.json',       // npm install
       'example/public/bower.json',  // bower install
     ])
@@ -67,13 +82,24 @@ gulp.task('install', function() {
 });
 
 gulp.task('default', [
-                      'install',
+                      'init',
                       'comp_tsconfig_files',
                       'comp_tsc',
                       'servercomp_tsconfig_files',
                       'servercomp_tsc',
                      ]);
 
-gulp.task('init');
-gulp.task('dev');
-gulp.task('start');
+gulp.task('init', [
+    'comp_tsconfig_files',
+    'comp_tsd',
+    'comp_tsconfig_files',
+    'comp_tsc',
+    'servercomp_tsd',
+    'servercomp_tsconfig_files',
+    'servercomp_tsc',
+    'example_deps',
+]);
+
+gulp.task('dev', ['?']);
+
+gulp.task('start', ['?']);
