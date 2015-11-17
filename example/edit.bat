16d9c64f7faecb3116139dffb91126ba37d5959e
Erik Vullings <erik.vullings@gmail.com>
2015-11-17 06:55:01 +0100

    Removed cesium from third party dependencies for now.
    It's a big download, and hardly used at all. Until it is better integrated, we should not use it.

--- example/edit.bat
+++
@@ -1,3 +0,0 @@
-start /B atom ./ ../csComp ../csServerComp ../test
-nodemon server.js
-start http://localhost:3002
--- example/test.js
+++
@@ -1,3 +0,0 @@
-var csweb = require('csweb');
-var manager = new csweb.api.ApiManager();
-manager.blabla();
