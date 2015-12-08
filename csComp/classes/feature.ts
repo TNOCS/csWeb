77feb39dad2060b446f5734ae6d8d28e2da7e07d
Erik Vullings <erik.vullings@gmail.com>
2015-12-08 08:02:42 +0100

    Feature._gui is now an Interface
    The only prop it will always have is 'included', which when true, means that the property is visible (as opposed to being filtered).

--- csComp/classes/feature.ts
+++ csComp/classes/feature.ts
@@ -17,8 +17,14 @@ module csComp.Services {
         user:  string;
     }
 
+    export interface IGuiObject {
+        /** When true, the feature is included on the map, as opposed to being removed by a filter. */
+        included: boolean;
+        [key: string]: any;
+    }    
+    
     export interface IFeature {
-        id?:               string;
+        id?:              string;
         index:            number;
         layerId:          string;
         layer:            ProjectLayer;
@@ -31,9 +37,9 @@ module csComp.Services {
         featureTypeName?: string;
         fType?:           IFeatureType;
         effectiveStyle:   IFeatureTypeStyle;
-        _isInitialized?: boolean;
+        _isInitialized?:  boolean;
         lastUpdated:      number;
-        _gui: Object;
+        _gui:             IGuiObject;
         sensors?:         { [id: string]: any[] }
 
         logs?:            { [id: string]: Log[] };
@@ -63,7 +69,7 @@ module csComp.Services {
         htmlStyle:       string;
         featureTypeName: string;
         lastUpdated:     number;
-        _gui: Object = {};
+        _gui:            IGuiObject = { included: true };
         /** resolved feature type */
         fType: IFeatureType;
         /** calculated style, used for final rendering */
@@ -227,6 +233,8 @@ module csComp.Services {
         name?:              string;
         style?:             IFeatureTypeStyle;
         legendItems?:       LegendList.ILegendItem[];
+        /** Optional expressions that are shown in the legend list. */
+        legendExpr?:        IPropertyType[];
         properties?:        {};
         _propertyTypeData?: IPropertyType[];
         showAllProperties?: boolean;
@@ -238,8 +246,6 @@ module csComp.Services {
          */
         propertyTypeKeys?: string;
         languages?:        ILanguageData;
-        /** Optional expressions that is/are shown in the legend list. */
-        expressions?:      string | string[];
         _isInitialized?:   boolean;
     }
 
