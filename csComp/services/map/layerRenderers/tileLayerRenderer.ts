76986deb75a8de0a854dae04f6f2b74743e6c7b0
Erik Vullings <erk.vullings@gmail.com>
2016-03-05 10:08:58 +0100

    Adding utfgrid to tilelayer renderer

--- csComp/services/map/layerRenderers/tileLayerRenderer.ts
+++ csComp/services/map/layerRenderers/tileLayerRenderer.ts
@@ -1,7 +1,8 @@
 module csComp.Services {
     export class TileLayerRenderer {
         static render(service: LayerService, layer: ProjectLayer) {
-            var u = layer.url;
+            var layers = layer.url.split('|');
+            var layerUrl = layers[0];
             if (layer.timeDependent) {
                 // convert epoch to time string parameter
                 var ft = service.project.timeLine.focus;
@@ -17,17 +18,37 @@ module csComp.Services {
                 var secs = d.getSeconds();
                 var sTime: string = Utils.twoDigitStr(hrs) +
                     Utils.twoDigitStr(mins) + Utils.twoDigitStr(secs);
-                u += '&time=' + sDate + sTime;
+                layerUrl += '&time=' + sDate + sTime;
             } else if (layer.disableCache) {
                 // check if we need to create a unique url to force a refresh
                 layer.cacheKey = new Date().getTime().toString();
-                u += '&cache=' + layer.cacheKey;
+                layerUrl += '&cache=' + layer.cacheKey;
             }
 
-            var tileLayer: any = L.tileLayer(u, { attribution: layer.description });
+            var tileLayer: any = L.tileLayer(layerUrl, { attribution: layer.description });
             layer.mapLayer = new L.LayerGroup<L.ILayer>();
             tileLayer.setOpacity(layer.opacity / 100);
             service.map.map.addLayer(layer.mapLayer);
+
+            if (layers.length > 1) {
+                var utfGrid = new (<any>L).UtfGrid(layers[1], {
+                    resolution: 4,
+                    useJsonP: false
+                });
+                utfGrid.on('click', function (e) {
+                    //click events are fired with e.data==null if an area with no hit is clicked
+                    if (e.data) {
+                        alert('click: ' + JSON.stringify(e.data, null, 2));
+                    } else {
+                        alert('click: nothing');
+                    }
+                });
+                utfGrid.on('mouseover', function (e) {
+                    console.log('hover: ' + JSON.stringify(e.data, null, 2));
+                });
+                service.map.map.addLayer(utfGrid);
+            }
+
             layer.mapLayer.addLayer(tileLayer);
             tileLayer.on('loading', (event) => {
                 layer.isLoading = true;
