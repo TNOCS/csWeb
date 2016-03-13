82169c498d7d5d45e627ef8f4ac6e5d9fd4adf97
Arnoud <damylen@gmail.com>
2016-03-13 10:48:52 +0100

    Extra nu check for Matrix property types

--- csComp/services/layer/MatrixAction.ts
+++ csComp/services/layer/MatrixAction.ts
@@ -35,18 +35,19 @@ module MatrixAction {
                 props.forEach((prop: IPropertyType) => {
                     if (prop.type === 'matrix' && feature.properties.hasOwnProperty(prop.label)) {
                         var m = feature.properties[prop.label];
-                        for (var f in m)
-                        {
+                        for (var f in m) {
                             var kb = m[f];
-                            if (kb.hasOwnProperty('b') && kb.b > 0  && kb.hasOwnProperty('h') && kb.h > 0)
-                            {
-                                var tf = this.layerService.findFeatureByPropertyValue(prop.targetid,f);
-                                var lh = prop.label + '_h';
-                                var lb = prop.label + '_b';
-                                if (!tf.properties.hasOwnProperty(lh)) tf.properties[lh] = 0;
-                                if (!tf.properties.hasOwnProperty(lb)) tf.properties[lb] = 0;
-                                tf.properties[lh] += kb.h;
-                                tf.properties[lb] += kb.b;
+                            if (kb.hasOwnProperty('b') && kb.b > 0 && kb.hasOwnProperty('h') && kb.h > 0) {
+                                var tf = this.layerService.findFeatureByPropertyValue(prop.targetid, f);
+                                if (tf) {
+                                    var lh = prop.label + '_h';
+                                    var lb = prop.label + '_b';
+
+                                    if (!tf.properties.hasOwnProperty(lh)) tf.properties[lh] = 0;
+                                    if (!tf.properties.hasOwnProperty(lb)) tf.properties[lb] = 0;
+                                    tf.properties[lh] += kb.h;
+                                    tf.properties[lb] += kb.b;
+                                }
                             }
                         }
                     }
