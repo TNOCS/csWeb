46048a234dd9c13e0d815fffba7bc1cf297ddf79
Arnoud <damylen@gmail.com>
2016-03-17 16:16:37 +0100

    added kpis property to sensorlink result

--- csComp/classes/datasource.ts
+++ csComp/classes/datasource.ts
@@ -3,6 +3,7 @@ module csComp.Services {
     export interface ISensorLinkResult {
         features : string[];
         timestamps: number[];
+        kpis :{[sensor : string ] : number[]}; 
         data: {[sensor : string ] : number[][]};
         properties: string[];
         timeAggregation: string;
