b52d5f2b5fecefe87760c563af637b1071884411
Erik Vullings <erik.vullings@gmail.com>
2016-04-26 14:30:49 +0300

    :lipstick:

--- csComp/directives/DashboardDirectives/Dashboard/DashboardCtrl.ts
+++ csComp/directives/DashboardDirectives/Dashboard/DashboardCtrl.ts
@@ -111,8 +111,7 @@ module Dashboard {
                         if (wScope && wScope.vm && wScope.vm.stop) {
                             wScope.vm.stop();
                         }
-                    } 
-                    catch (e) { }
+                    } catch (e) { }
                 }
             });
         }
@@ -347,32 +346,8 @@ module Dashboard {
                 w.width = '150px';
                 w.enabled = true;
                 d.widgets.push(w);
-                //this.$dashboardService.
-                //this.$dashboardService.addNewWidget(w, d);
-                //this.$dashboardService.selectDashboard(this.$layerService.project.activeDashboard, 'main');
             }
         };
-
-        // if (!d.widgets) d.widgets = [];
-        // if (d.showLegend) {
-        //     var legendWidgetPresent = false;
-        //     d.widgets.forEach(w => {
-        //         if(w.id === 'legend') legendWidgetPresent = true;
-        //     });
-        //     if (!legendWidgetPresent) {
-        //         console.log('Create legend');
-        //         var w = <csComp.Services.IWidget>{};
-        //         w.directive = 'legend-directive';
-        //         w.id = 'legend';
-        //         w.title = 'Legenda';
-        //         w.data = {mode: 'lastSelectedStyle'};
-        //         w.left = '10px';
-        //         w.top = '20px';
-        //         w.width = '150px';
-        //         w.enabled = true;
-        //         this.$dashboardService.addNewWidget(w, d);
-        //         //this.$dashboardService.selectDashboard(this.$layerService.project.activeDashboard, 'main');
-        //     }
 
         public updateDashboard() {
             var d = this.$scope.dashboard;
