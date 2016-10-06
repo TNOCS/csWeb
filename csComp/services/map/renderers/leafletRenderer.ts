39ce4be28648c027ec626b866bfa01b0f159f0e6
Arnoud <damylen@gmail.com>
2016-10-06 08:32:30 +0200

    Added helper code voor RD projection

--- csComp/services/map/renderers/leafletRenderer.ts
+++ csComp/services/map/renderers/leafletRenderer.ts
@@ -27,10 +27,22 @@ module csComp.Services {
                 this.enableMap();
                 return;
             }
+            // var res = [3440.640, 1720.320, 860.160, 430.080, 215.040, 107.520, 53.760, 26.880, 13.440, 6.720, 3.360, 1.680, 0.840, 0.420];
+
+
+
+            // var RD = new L.Proj.CRS('EPSG:28992', '+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +towgs84=565.2369,50.0087,465.658,-0.406857330322398,0.350732676542563,-1.8703473836068,4.0812 +no_defs',
+            //     {
+            //         resolutions: [3440.640, 1720.320, 860.160, 430.080, 215.040, 107.520, 53.760, 26.880, 13.440, 6.720, 3.360, 1.680, 0.840, 0.420],
+            //         bounds: L.bounds([-285401.92, 22598.08], [595401.9199999999, 903401.9199999999]),
+            //         origin: [-285401.92, 22598.08]
+            //     }
+            // );
             var mapOptions: L.Map.MapOptions = {
                 zoomControl: false,
                 maxZoom: 22,
                 attributionControl: true
+                //,crs: RD
             };
             this.map = this.service.$mapService.map = L.map('map', mapOptions);
 
@@ -178,7 +190,7 @@ module csComp.Services {
             this.map.fire('baselayerchange', { layer: this.baseLayer });
         }
 
-        private createBaseLayer(layerObj: BaseLayer) : L.TileLayer {
+        private createBaseLayer(layerObj: BaseLayer): L.TileLayer {
             var options: L.TileLayerOptions = { noWrap: true };
             options['subtitle'] = layerObj.subtitle;
             options['preview'] = layerObj.preview;
@@ -360,7 +372,7 @@ module csComp.Services {
             var marker;
             switch (feature.geometry.type) {
                 case 'Point':
-                    if (!feature.geometry.coordinates || feature.geometry.coordinates.length<2 || isNaN(feature.geometry.coordinates[0]) || isNaN(feature.geometry.coordinates[1])) return;
+                    if (!feature.geometry.coordinates || feature.geometry.coordinates.length < 2 || isNaN(feature.geometry.coordinates[0]) || isNaN(feature.geometry.coordinates[1])) return;
                     var icon = this.getPointIcon(feature);
 
                     marker = new L.Marker(new L.LatLng(feature.geometry.coordinates[1], feature.geometry.coordinates[0]), {
@@ -420,7 +432,7 @@ module csComp.Services {
                         marker.on('contextmenu', (e: any) => {
                             this.service._activeContextMenu = this.service.getActions(feature, ActionType.Context);
                             if (!this.service._activeContextMenu || this.service._activeContextMenu.length === 0) return;
-                            
+
                             //e.stopPropagation();
                             var button: any = $('#map-contextmenu-button');
                             var menu: any = $('#map-contextmenu');
