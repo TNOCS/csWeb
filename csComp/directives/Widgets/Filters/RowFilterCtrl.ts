9ef55f46506b7baf064f2c4079fef419cbbc5bde
rinze <rinze.bruining@tno.nl>
2016-12-29 13:18:14 +0100

    Restore original rowfilter

--- csComp/directives/Widgets/Filters/RowFilterCtrl.ts
+++ csComp/directives/Widgets/Filters/RowFilterCtrl.ts
@@ -149,13 +149,9 @@ module Filters {
                             } else if (pt.type === 'text' || pt.type === 'textarea' || pt.type === 'textarea-right') {
                                 return a;
                             }
-                            return label;
-                        } else if (pt.type === 'text' || pt.type === 'textarea' || pt.type === 'textarea-right') {
-                            return a;
                         }
+                        return null;
                     }
-                    return null;
-                }
             });
             filter.dimension = dcDim;
             var dcGroup = dcDim.group();
@@ -169,23 +165,9 @@ module Filters {
                     return d;
                 });
                 var fakeGroup = fakeDim.group();
-
-                //Sort ordering
-                pt.legend.legendEntries.forEach((le) => {
-                    if (le.hasOwnProperty('sortKey')) {
-                        let sk = le.sortKey;
-                        if (!orderList.hasOwnProperty(sk)) {
-                            orderList[le.label] = {nr: Object.keys(orderList).length, sortKey: sk};
-                        }
-                    }
-                });
-                var sortKeys = _.map(orderList, (item) => { return item.sortKey});
-                sortKeys = sortKeys.sort();
-                _.each(orderList, (val, key) => { val.nr = sortKeys.indexOf(val.sortKey)});
             }
 
             var ensuredGroup = (fakeGroup ? this.ensureAllBins(dcGroup, fakeGroup) : null);
-            var h = (ensuredGroup && ensuredGroup.size() < 6) ? 180 : 250;
 
             this.dcChart.width(380)
                 .height(285)
@@ -197,24 +179,10 @@ module Filters {
                 .elasticX(true)
                 .colors(d=>{
                     if (pt && pt.legend) {
-                        if (pt.options) {
-                            return csComp.Helpers.getColorFromLegend(d, pt.legend);
-                        } else if (pt.type === 'number' && !isNaN(d)) {
-                            var arr = pt.legend.legendEntries.filter((le) => {
-                                return (d >= le.interval.min && le.interval.max >= d);
-                            });
-                            if (arr.length > 0) {
-                                return arr[0].color.substring(0, 7);
-                            } else {
-                                if (pt && pt.legend && pt.legend.hasOwnProperty('defaultLabel')) {
-                                    return pt.legend.legendEntries.find((le) => { return le.label === pt.legend.defaultLabel; });
-                                } else {
-                                    return '#444444';
-                                }
-                            }
-                        } else if (!pt.options) {
+                        if (pt.options) return csComp.Helpers.getColorFromLegend(parseInt(d.split('.')[0]), pt.legend);
+                        if (!pt.options) {
                             var arr = pt.legend.legendEntries.filter((le => { return le.label === d }));
-                            return (arr.length > 0 ? arr[0].color.substring(0, 7) : '#444444');
+                            return (arr.length > 0 ? arr[0].color : '#444444');
                         }
                     }
                     else {
@@ -222,29 +190,13 @@ module Filters {
                     }
                 })
                 .cap(10)
-                .ordering(d => {
-                    if (pt && pt.legend) {
-                        if (orderList.hasOwnProperty(d.key)) {
-                            return orderList[d.key].nr;
-                        } else {
-                            return d.key || d.value;
-                        }
-                    } else {
-                        return d.key || d.value;
-                    }
-                })
                 .on('renderlet', (e) => {
                     dc.events.trigger(() => {
                         this.$layerService.updateFilterGroupCount(group);
                     }, 0);
                     dc.events.trigger(() => {
-                        console.log('RowFilterCtrl: renderlet');
-                        if (dcDim && dcDim.top(Infinity).length > 0 && typeof dcDim.top(Infinity)[0] === 'undefined') {
-                            console.log('dcDim contains undefined objects');
-                        } else {
-                            group.filterResult = dcDim.top(Infinity);
-                            this.$layerService.updateMapFilter(group);
-                        }
+                        group.filterResult = dcDim.top(Infinity);
+                        this.$layerService.updateMapFilter(group);
                     }, 100);
                 })
                 .on('filtered', (e) => {
@@ -253,6 +205,7 @@ module Filters {
             this.dcChart.xAxis().ticks(8);
             this.dcChart.selectAll();
             this.updateRange();
+            dc.renderAll();
         }
 
         private ensureAllBins(source_group, fake_group) { // (source_group, bins...}
@@ -290,28 +243,21 @@ module Filters {
 
         private updateFilter() {
             setTimeout(() => {
-                console.log('RowFilterCtrl: updateFilter()');
-                if (this.$scope.filter.filterLabel) {
-                    this.dcChart.filter(this.$scope.filter.filterLabel);
-                }
+                this.dcChart.filter(this.$scope.filter.filterLabel);
                 this.dcChart.render();
                 dc.renderAll();
                 this.$layerService.updateMapFilter(this.$scope.filter.group);
-            }, 20);
+            }, 10);
         }
 
         public updateRange() {
             setTimeout(() => {
-                console.log('RowFilterCtrl: updateRange()');
                 var filter = this.$scope.filter;
                 var group = filter.group;
                 this.dcChart.filterAll();
-                if (this.$scope.filter.filterLabel) {
-                    this.dcChart.filter(this.$scope.filter.filterLabel);
-                }
+                this.dcChart.filter(this.$scope.filter.filterLabel);
                 this.dcChart.render();
-                // dc.redrawAll();
-                // dc.renderAll();
+                dc.redrawAll();
                 group.filterResult = filter.dimension.top(Infinity);
                 this.$layerService.updateMapFilter(this.$scope.filter.group);
                 this.$layerService.triggerUpdateFilter(this.$scope.filter.group.id);
