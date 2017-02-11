f419f4f7c68361c765c8c81fac7ac9987a132806
Erik Vullings <erik.vullings@gmail.com>
2017-02-11 21:20:13 +0100

    Added Util: remove folder recursively.

--- csServerComp/ServerComponents/helpers/Utils.ts
+++ csServerComp/ServerComponents/helpers/Utils.ts
@@ -1,9 +1,30 @@
-var fs = require('fs'),
-    path = require('path');
+import * as fs from 'fs';
+import * as path from 'path';
+
+
+/**
+ * Delete a folder synchronously.
+ * Source: http://stackoverflow.com/a/32197381/319711
+ *
+ * @export
+ * @param {string} folder
+ */
+export function deleteFolderRecursively(folder: string) {
+    if (!fs.existsSync(folder)) { return; }
+    fs.readdirSync(folder).forEach(function (file, index) {
+        var curPath = folder + '/' + file;
+        if (fs.lstatSync(curPath).isDirectory()) { // recurse
+            deleteFolderRecursively(curPath);
+        } else { // delete file
+            fs.unlinkSync(curPath);
+        }
+    });
+    fs.rmdirSync(folder);
+};
 
 /** Create a new GUID */
 export function newGuid() {
-    var guid = (this.S4() + this.S4() + "-" + this.S4() + "-4" + this.S4().substr(0, 3) + "-" + this.S4() + "-" + this.S4() + this.S4() + this.S4()).toLowerCase();
+    var guid = (this.S4() + this.S4() + '-' + this.S4() + '-4' + this.S4().substr(0, 3) + '-' + this.S4() + '-' + this.S4() + this.S4() + this.S4()).toLowerCase();
     return guid;
 }
 
@@ -13,16 +34,16 @@ export function S4() {
 
 /** Get all the directories in a folder. */
 export function getDirectories(srcpath: string): string[] {
-  return fs.readdirSync(srcpath).filter(function(file: string) {
-    return fs.statSync(path.join(srcpath, file)).isDirectory();
-  });
+    return fs.readdirSync(srcpath).filter(function (file: string) {
+        return fs.statSync(path.join(srcpath, file)).isDirectory();
+    });
 }
 
 /** Get all the files in a folder. */
 export function getFiles(srcpath: string): string[] {
-  return fs.readdirSync(srcpath).filter(function(file: string) {
-    return fs.statSync(path.join(srcpath, file)).isFile();
-  });
+    return fs.readdirSync(srcpath).filter(function (file: string) {
+        return fs.statSync(path.join(srcpath, file)).isFile();
+    });
 }
 
 /**
@@ -30,16 +51,16 @@ export function getFiles(srcpath: string): string[] {
  * See also: http://stackoverflow.com/a/15075395/319711
  */
 export function getIPAddress(): string {
-  var interfaces = require('os').networkInterfaces();
-  for (var devName in interfaces) {
-    var iface = interfaces[devName];
+    var interfaces = require('os').networkInterfaces();
+    for (var devName in interfaces) {
+        var iface = interfaces[devName];
 
-    for (var i = 0; i < iface.length; i++) {
-      var alias = iface[i];
-      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal)
-        return alias.address;
+        for (var i = 0; i < iface.length; i++) {
+            var alias = iface[i];
+            if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal)
+                return alias.address;
+        }
     }
-  }
 
-  return '0.0.0.0';
+    return '0.0.0.0';
 }
